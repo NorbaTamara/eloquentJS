2e89bf1308f47bbf55566a2f622f586b5d0384b6
Tamara Norba <norba.tamara@gmail.com>
2020-04-12 21:09:04 +0300

    Recursion&beancounting tasks from Functions chapter

---
+++ functions_recursion_bean_counting.js
@@ -0,0 +1,27 @@
+// https://eloquentjavascript.net/03_functions.html
+// Recursion
+function isEven(number) {
+    const absoluteNumber = Math.abs(number);
+    if (absoluteNumber === 0) {
+      return true; 
+    } else if (absoluteNumber === 1) {
+      return false;
+    } else {
+      return isEven(absoluteNumber - 2);
+    }
+  }
+  console.log(isEven(50));
+  console.log(isEven(75));
+  console.log(isEven(-50));
+
+  // Bean counting
+  function countChar(string, countedChar) {
+    let counter = 0;
+    for (let i = 0; i < string.length; i++) {
+      if(string[i] === countedChar) {
+         counter += 1;
+      }
+    }
+    return counter;
+  }
+  console.log(countChar("kakDASCDadsdbkkkdgfgkerlak", "f"));
---
+++ program_structure_chessboard.js
@@ -0,0 +1,21 @@
+// https://eloquentjavascript.net/02_program_structure.html
+// Chessboard
+function chessBoard(size){
+    let board = '';
+    let lines = '';
+    for(linesCount = 0; linesCount < size; linesCount++){
+        let line = '';
+        for(columnsCount = 0; columnsCount < size; columnsCount++){
+            let cell = '#';
+            if (linesCount%2 !== 0 && columnsCount%2 !== 0
+                ||linesCount%2 === 0 && columnsCount%2 === 0){
+                    cell = ' ';
+                }
+            line = line + cell;
+        }
+      board = board + line + '\n';
+    }
+    return board;
+}
+
+console.log(chessBoard(10));
