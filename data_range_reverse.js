bb34bc6ed35c09d6a10e8c9d3f9dbd0c57cacc6a
Tamara Norba <norba.tamara@gmail.com>
2020-04-27 10:39:49 +0300

    Another variant of range function

--- data_range_reverse.js
+++ data_range_reverse.js
@@ -21,6 +21,22 @@ function range(start, end, step = 1) {
   return range;
 }
 
+// to check
+function range(start, end, step) {
+  if ((start > end && step > 0) || (start < end && step < 0) || step == 0) return [];
+  var result = [start];
+  var current = start;
+  while (true) {
+    current = current + step;
+    if (step > 0 && current > end) break;
+    if (step < 0 && current < end) break;
+    result.push(current);
+  }
+  return result;
+}
+// to check
+
+
 function sum (range) {
   let sum = 0;
   range.forEach(number => {
@@ -43,10 +59,9 @@ function reverseArray(array){
 }
 
 function reverseArrayInPlace(array){
-  for (let i = 0; i < Math.floor(array.length/2); i++) {
-    const memorizedElement = array[i];
-    array[i] = array[(array.length-1) - i];
-    array[(array.length-1) - i] = memorizedElement;
+  const initialArray = [...array];
+  for (let i = 0; i < array.length; i++) {
+    array[i] = initialArray[(initialArray.length-1) - i]
   }
   return array;
 }
